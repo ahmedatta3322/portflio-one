69d2b8e88d3db261b952e82ce0316c20bad0bdb5
ahmedatta3322 <ahmedatta3322@gmail.com>
2018-09-19 01:49:29 +0300

    make the form of submitting new portoflio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,15 @@ class PortfoliosController < ApplicationController
     @port_items = Portfolio.all
     #code
   end
+  def new
+    @port_items = Portfolio.new
+    #code
+  end
+  def create
+    @port_items = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
+    if @port_items.save
+      redirect_to portfolios_path
+    end
+
+  end
 end
