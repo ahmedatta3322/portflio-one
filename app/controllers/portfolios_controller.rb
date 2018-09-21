0b6cc562afd8fbb1a701ade9244102678c71268a
ahmedatta3322 <ahmedatta3322@gmail.com>
2018-09-21 19:54:51 +0300

    add edit and update

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,10 @@
 class PortfoliosController < ApplicationController
+  def params_def
+    return params.require(:portfolio).permit(:title,:subtitle,:body)
+  end
+  def find_current
+    @port_items = Portfolio.find(params[:id])
+  end
   def index
     @port_items = Portfolio.all
     #code
@@ -7,11 +13,23 @@ class PortfoliosController < ApplicationController
     @port_items = Portfolio.new
     #code
   end
+  def edit
+    @port_items = find_current
+  end
+  def update
+    @port_items = find_current
+    if @port_items.update(params_def)
+      redirect_to portfolios_path
+    end
+    #code
+  end
   def create
-    @port_items = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
+    @port_items = Portfolio.new(params_def)
     if @port_items.save
       redirect_to portfolios_path
     end
 
+
   end
+
 end
